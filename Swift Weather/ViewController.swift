48b0f21c25008eed508840800292e01cba3b5034
Jerry Yao <yaojia.smile@gmail.com>
2014-09-17 23:50:03 +0800

    GPS location working OK

--- Swift Weather/ViewController.swift
+++ Swift Weather/ViewController.swift
@@ -7,17 +7,45 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager:CLLocationManager = CLLocationManager()
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+  
+        
+        if (ios8()) {
+            locationManager.requestAlwaysAuthorization()
+        }
+        locationManager.startUpdatingLocation()
     }
 
+    func ios8() -> Bool {
+        return UIDevice.currentDevice().systemVersion == "8.0"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var location:CLLocation = locations[locations.count - 1] as CLLocation
+        if (location.horizontalAccuracy > 0) {
+            println(location.coordinate.latitude)
+            println(location.coordinate.longitude)
+            
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        println(error)
     }
 
 
