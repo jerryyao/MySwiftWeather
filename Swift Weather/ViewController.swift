196c0fcd6166d5058230bba6fd1052be7a86c423
Jerry Yao <yaojia.smile@gmail.com>
2014-09-17 22:45:24 +0800

    Initial Commit

---
+++ Swift Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Weather
+//
+//  Created by 姚嘉 on 14-9-17.
+//  Copyright (c) 2014年 yaojia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
