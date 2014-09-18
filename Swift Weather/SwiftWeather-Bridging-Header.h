2edd971d6c2e7693c986f3f12eb349bfadf57d8e
Jerry Yao <yaojia.smile@gmail.com>
2014-09-18 15:52:56 +0800

    fully completed based on the web lecture "Swift Weather"

---
+++ Swift Weather/SwiftWeather-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SwiftWeather-Bridging-Header.h
+//  Swift Weather
+//
+//  Created by 姚嘉 on 14-9-17.
+//  Copyright (c) 2014年 yaojia. All rights reserved.
+//
+
+#ifndef Swift_Weather_SwiftWeather_Bridging_Header_h
+#define Swift_Weather_SwiftWeather_Bridging_Header_h
+
+#import <AFNetworking/AFNetworking.h>
+
+#endif
